5b9e91569730c94586e64178f7c44463ed9bb90d
DX2DigitalGroup <dbuihien@my.bcit.ca>
2019-05-26 13:20:28 -0700

    added files

---
+++ gitrepos.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#git clone git@github.com:DX2DigitalGroup.git
+git clone git@github.com:DX2DigitalGroup/DX2Googler.git
+
---
+++ pkg-install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+pkg update -y
+pkg install toilet tree less nano byobu apt asciinema bash bmon busybox figlet fortune gawk git grep htop make openssh python* 
